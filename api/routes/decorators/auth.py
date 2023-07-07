1f34c46702696e601f58f55e99e77debddf20223
Bienvenido Villabroza <benv@thinkingmachin.es>
2023-07-07 09:20:26 +0000

    protect endpoints by requiring authentication with decorator

---
+++ api/routes/decorators/auth.py
@@ -0,0 +1,61 @@
+from functools import wraps
+from firebase_admin import auth
+from firebase_admin.auth import ExpiredIdTokenError
+from fastapi.exceptions import HTTPException
+
+
+def require_authentication(func):
+    @wraps(func)
+    async def wrapper(*args, **kwargs):
+        request = kwargs.get("request", None)
+        if not request:
+            raise HTTPException(
+                detail={
+                    "message": f"The endpoint must have the client 'request' as a parameter"
+                },
+                status_code=400,
+            )
+
+        token = request.headers.get("Authorization", None)
+        if not token:
+            raise HTTPException(
+                detail={
+                    "message": f"This endpoint requires authentication. You are missing an Authorization header"
+                },
+                status_code=401,
+            )
+
+        try:
+            auth.verify_id_token(token)
+        except ExpiredIdTokenError:
+            raise HTTPException(
+                detail={
+                    "message": "Your token is expired. Login again",
+                    "action": "login",
+                },
+                status_code=400,
+            )
+        except Exception as exception:
+            raise HTTPException(
+                detail={
+                    "message": f"There was an error validating the token: {str(exception)}"
+                },
+                status_code=400,
+            )
+
+        return await func(*args, **kwargs)
+
+        # make synchronous endpoints work
+        # try:
+        #     return await func(*args, **kwargs)
+        # except TypeError:
+        #     return func(*args, **kwargs)
+        # except Exception as exception:
+        #     raise HTTPException(
+        #         detail={
+        #             "message": f"There was an returning the inner fucntion: {str(exception)}"
+        #         },
+        #         status_code=400,
+        #     )
+
+    return wrapper
--- api/routes/emotion.py
+++ api/routes/emotion.py
@@ -1,5 +1,6 @@
+from .decorators.auth import require_authentication
 from db.database import get_db
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from typing import List
 from sqlalchemy.orm import Session
 from fastapi import Depends
@@ -11,15 +12,22 @@ router = APIRouter(prefix="/emotion", tags=["emotion"])
 
 
 @router.post("/create", response_model=EmotionResponse)
-def create_emotion(emotion: EmotionCreateRequest, db: Session = Depends(get_db)):
+@require_authentication
+async def create_emotion(
+    request: Request, emotion: EmotionCreateRequest, db: Session = Depends(get_db)
+):
     return emotion_op.create_emotion(db, emotion)
 
 
 @router.post("/read", response_model=List[EmotionResponse])
-def read_emotion(emotion: EmotionReadRequest, db: Session = Depends(get_db)):
+@require_authentication
+async def read_emotion(
+    request: Request, emotion: EmotionReadRequest, db: Session = Depends(get_db)
+):
     return emotion_op.read_emotion(db, emotion)
 
 
 @router.get("/read/all", response_model=List[EmotionResponse])
-def read_emotion_all(db: Session = Depends(get_db)):
+@require_authentication
+async def read_emotion_all(request: Request, db: Session = Depends(get_db)):
     return emotion_op.read_emotion_all(db)
