7c628302955435a228e7bf5fb52e2c2e783b4321
tm-ben-villabroza <benv@thinkingmachin.es>
2023-07-19 15:48:27 +0800

    add simple chat/notif websocket endpoint

--- api/routes/websocket.py
+++ api/routes/websocket.py
@@ -1,11 +1,11 @@
-from fastapi import WebSocket, APIRouter
+from fastapi import WebSocket, APIRouter, WebSocketDisconnect
 from datetime import datetime
 import time
 
 router = APIRouter(prefix="/websocket", tags=["websocket"])
 
 @router.websocket("/send-receive-messages-to-server")
-async def websocket_endpoint_basic(websocket: WebSocket):
+async def websocket_basic(websocket: WebSocket):
     await websocket.accept()
 
     while True:
@@ -16,8 +16,8 @@ async def websocket_endpoint_basic(websocket: WebSocket):
 
         await websocket.send_text(f"Message text was: {data}")
 
-@router.websocket("/receive-notifs")
-async def websocket_endpoint_basic(websocket: WebSocket):
+@router.websocket("/receive-notifs-hardcoded")
+async def websocket_receive_notifs_hardcoded(websocket: WebSocket):
     await websocket.accept()
 
     notif_queue = [
@@ -25,12 +25,48 @@ async def websocket_endpoint_basic(websocket: WebSocket):
         "Your account has been hacked 😔",
         "Your character just reached level 3 🥳",
         "Your character just reached level 2 🥳",
-        "You account has been successfully created 🥳",
+        "Your account has been successfully created 🥳",
         ]
 
     while True:
-        if notif_queue:
-            await websocket.send_text(f"New notif: {notif_queue.pop()}")
+        await websocket.send_text(f"New notif: {notif_queue.pop()}")
 
         # await for new notifs
         time.sleep(2)
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
+manager = ConnectionManager()
+
+
+
+@router.websocket("/notifs/")
+async def websocket_endpoint(websocket: WebSocket):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_json()
+            print(data)
+            await manager.send_personal_message(f"You wrote: {data['message']}", websocket)
+            await manager.broadcast(f"Client {data['username']} says: {data['message']}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client {data['username']} left the chat")
