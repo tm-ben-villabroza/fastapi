4241672c0289316410136bb1ef3c7cce42380064
tm-ben-villabroza <benv@thinkingmachin.es>
2023-07-19 13:59:24 +0800

    rename endpoint

--- api/routes/websocket.py
+++ api/routes/websocket.py
@@ -1,10 +1,17 @@
 from fastapi import WebSocket, APIRouter
+from datetime import datetime
+import time
 
 router = APIRouter(prefix="/websocket", tags=["websocket"])
 
-@router.websocket("/")
+@router.websocket("/send-receive-messages-to-server")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
+
     while True:
         data = await websocket.receive_text()
+
+        # long running task
+        time.sleep(1)
+
         await websocket.send_text(f"Message text was: {data}")
