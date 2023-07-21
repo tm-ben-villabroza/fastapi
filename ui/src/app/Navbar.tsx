eb906af5af56cca0e67f4262bd3c1d6a258b5b6e
tm-ben-villabroza <benv@thinkingmachin.es>
2023-07-21 12:32:37 +0800

    working skeleton with nav bar

---
+++ ui/src/app/Navbar.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import React from "react";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu";
+
+function Navbar() {
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <NavigationMenuLink>Link</NavigationMenuLink>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
+
+export default Navbar;
