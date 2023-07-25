2b288641bde60a25b3be3649a2cb72c105a9ca60
tm-ben-villabroza <benv@thinkingmachin.es>
2023-07-25 09:32:10 +0800

    use postgres for local dev

--- api/db/database.py
+++ api/db/database.py
@@ -2,11 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./fastapi-test.db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./fastapi-test.db"
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+# )
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@localhost:5432/postgres"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 BaseModel = declarative_base()
