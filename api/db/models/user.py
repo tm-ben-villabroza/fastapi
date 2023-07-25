6cc7cda20828d979fd04f51001ccceba3e464cef
tm-ben-villabroza <benv@thinkingmachin.es>
2023-07-25 11:55:42 +0800

    add cascade on delete to user_permissions FKs

--- api/db/models/user.py
+++ api/db/models/user.py
@@ -9,8 +9,12 @@ from ..database import BaseModel
 user_permissions = Table(
     "user_permissions",
     BaseModel.metadata,
-    Column("user_id", ForeignKey("users.id"), primary_key=True),
-    Column("permission_id", ForeignKey("permissions.id"), primary_key=True),
+    Column("user_id", ForeignKey("users.id", ondelete="CASCADE"), primary_key=True),
+    Column(
+        "permission_id",
+        ForeignKey("permissions.id", ondelete="CASCADE"),
+        primary_key=True,
+    ),
 )
 
 
